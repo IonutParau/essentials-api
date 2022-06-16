b8013b38c3a629b20a01a2aab1794dfe66cf2dc3
IonutParau <ionut.alex.parau@gmail.com>
2022-06-16 20:27:47 +0200

    Adding Synapse connections

--- Mods/Synapse/main.lua
+++ Mods/Synapse/main.lua
@@ -4,6 +4,40 @@ Synapse = {}
 
 Synapse.channels = {}
 
+local _connections = {}
+
+-- Creates a port to be used for completely localized trusted connections
+---@param id string The ID of the port
+---@param listener function The function called by the connections. What this returns, the connection will receive.
+---@param filter function This function gets 1 parameter, the connection data, which is optional. If it returns true, the connection will be accepted. Otherwise, it will be closed.
+function Synapse.createConnectable(id, listener, filter)
+  local t = {
+    listener = listener,
+    filter = filter or function() return true end,
+  }
+
+  _connections[id] = id
+end
+
+-- Connect using the ID to the connection port
+---@param id string
+---@param connectionData any|nil Optional
+function Synapse.connect(id, connectionData)
+  if _connections[id] == nil then
+    return function() end
+  end
+
+  local c = _connections[id]
+
+  if c.filter(connectionData) == true then
+    return function(...)
+      return unpack { c.listener(...) }
+    end
+  else
+    return function() end
+  end
+end
+
 function Synapse.NewChannel(name)
   Synapse.channels[name] = {}
 end
